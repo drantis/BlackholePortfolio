d4dbe810193775b2307e2e8dcc843fc9f0a25e68
drantis <drantis@live.com>
2024-01-13 17:04:42 -0500

    Formatting
    - formatted globals.css, layout.tsx, page.tsx, Hero.tsx and HeroContent.tsx using Prettier formatter

--- 3dspaceportfolio/components/main/Hero.tsx
+++ 3dspaceportfolio/components/main/Hero.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import HeroContent from '../sub/HeroContent'
+import React from "react";
+import HeroContent from "../sub/HeroContent";
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
-        <video
+    <div className="relative flex flex-col h-full w-full">
+      <video
         muted
         autoPlay
         loop
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
-        >
-        <source src='blackhole.webm' type='video/webm'/>
-        </video>
-        <HeroContent/>
+        className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+      >
+        <source src="blackhole.webm" type="video/webm" />
+      </video>
+      <HeroContent />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
