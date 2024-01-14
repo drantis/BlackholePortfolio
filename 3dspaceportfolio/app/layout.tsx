f85749fec828fc4b5c7a5f43b4d3f2fdcb1fc881
drantis <drantis@live.com>
2024-01-14 14:18:17 -0500

    Footer Section Integration + Fix Vercel Deployment error

--- 3dspaceportfolio/app/layout.tsx
+++ 3dspaceportfolio/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +23,9 @@ export default function RootLayout({
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas />
-        <Navbar/>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- README.md
+++
@@ -1,5 +0,0 @@
-# Blackhole
-Blackhole is a space themed portfolio website created using latest 3D web technologies
-- Visit http://localhost:3000 in your browser to access the website
-
-If you have any questions or comments about Blackhole, please feel free to contact me at drantis@live.com
