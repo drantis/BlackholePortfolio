2439d524377313ed4bfb31151695e273765421f5
drantis <drantis@live.com>
2024-04-11 14:59:38 -0400

    Forcing desktop view

--- 3dspaceportfolio/app/layout.tsx
+++ 3dspaceportfolio/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
@@ -19,6 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Force desktop layout */}
+        <meta name="viewport" content="width=1024" />
+      </Head>
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
