ab97b8c013ed6412b20740b06d7fd90fff33e8c1
drantis <drantis@live.com>
2024-04-11 16:03:27 -0400

    Reverting changes

--- 3dspaceportfolio/app/layout.tsx
+++ 3dspaceportfolio/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState, ReactNode } from "react";
-import Head from "next/head";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
@@ -16,23 +15,14 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 
   useEffect(() => {
     const updateWidth = () => {
-      const currentWidth = `${window.innerWidth}px`;
-      setMinWidth(currentWidth);
+      const maxWidth = Math.max(window.innerWidth, window.innerHeight) + "px";
+      setMinWidth(maxWidth);
     };
-
-    window.addEventListener("resize", updateWidth);
     updateWidth();
-
-    return () => {
-      window.removeEventListener("resize", updateWidth);
-    };
   }, []);
 
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width" />
-      </Head>
       <body
         className="bg-[#030014] overflow-y-scroll overflow-x-hidden"
         style={{ minWidth }}
